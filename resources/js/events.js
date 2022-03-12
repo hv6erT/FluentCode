ecfa8ac292ea53f1e3ea9d27b3c65bcc084ee972
hv6erT <92681087+hv6erT@users.noreply.github.com>
2022-03-12 22:20:27 +0100

    Updated error handling
    Add error handling in close function

--- resources/js/events.js
+++ resources/js/events.js
@@ -30,10 +30,27 @@ window.addEventListener("blur", async function () {
 
 Neutralino.events.on("windowClose", async function() {
   if(settings.settings.file["save-before-close"] === true)
-    await FileManager.saveAllFiles();
+    try{await FileManager.saveAllFiles();}catch(error){
+      const errorResponse = await Neutralino.os.showMessageBox("Error: Files could not be saved", `Error message: "${error.message}"`, "ABORT_RETRY_IGNORE", "ERROR");
+
+      if(errorResponse === "ABORT"){
+        const savingConfirmResponse = await Neutralino.os.showMessageBox("Confirm close without save", `Are you sure you want to close Fluent Code? Your work and other changes would not be saved."`, "YES_NO", "QUESTION");
+
+        if(savingConfirmResponse === "YES")
+          await Neutralino.app.exit();
+        else return;
+      }
+      else if(errorResponse === "RETRY")
+        return Neutralino.events.dispatch("windowClose");
+    }
 
   if(settings.hasChanged() === true)
-    await Neutralino.filesystem.writeFile(userPreferences.settingsFilePath, JSON.stringify(settings.userSettings));
+    try{await Neutralino.filesystem.writeFile(userPreferences.settingsFilePath, JSON.stringify(settings.userSettings));}catch(error){
+      const settingsSaveConfirmResponse = await Neutralino.os.showMessageBox("Error: Settings could not be saved", `Settings could not be saved, do you want to close anyway? \n Error message: "${error.message}"`, "YES_NO", "ERROR");
+
+      if(settingsSaveConfirmResponse === "NO")
+        return;
+    }
 
   try{
     await Storage.saveFileKeys();
