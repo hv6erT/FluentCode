91fb649141849a2e65d31e7a5159b352ab941a0f
hv6erT <92681087+hv6erT@users.noreply.github.com>
2022-05-09 09:30:39 +0200

    Change build script
    Change build script for better deploing new builds. Add support for future feature - auto update.

--- build/build.js
+++ build/build.js
@@ -1,10 +1,31 @@
-const childProcess = require("child_process");
-const rcedit = require("rcedit");
+import childProcess from "child_process";
+import fileSystem from "fs";
 
+import rcedit from "rcedit";
+
+const appInfo = JSON.parse(await fileSystem.promises.readFile("./../neutralino.config.json"));
+
+//Step 1. Build neutralino app
 childProcess.execSync("neu build", {
   cwd: "../"
 });
 
-rcedit("../dist/fluentcode/fluentcode-win_x64.exe", {
+//Step 2. Rename binary files
+await fileSystem.promises.rename("../dist/fluentcode/fluentcode-win_x64.exe", "../dist/fluentcode/Fluent Code.exe");
+await fileSystem.promises.rename("../dist/fluentcode/fluentcode-linux_x64", "../dist/fluentcode/Fluent Code for Linux");
+await fileSystem.promises.rename("../dist/fluentcode/fluentcode-mac_x64", "../dist/fluentcode/Fluent Code for Mac OS");
+
+//Step 3. Change icons of binary files
+rcedit("../dist/fluentcode/Fluent Code.exe", {
   icon: "../resources/favicon.ico"
 });
+
+//Step 4. Create update file
+const updateInfo = {
+  applicationId: appInfo.applicationId,
+  version: appInfo.version,
+  resourcesURL: `https://raw.githubusercontent.com/hv6erT/FluentCode/main/dist/fluentcode/resources.neu`,
+  data: {}
+};
+
+await fileSystem.promises.writeFile("../dist/update.json", JSON.stringify(updateInfo, null, 4));
