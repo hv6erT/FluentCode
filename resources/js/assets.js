8100f502c628ddc198a2241f4f6061f995c8ce86
hv6erT <92681087+hv6erT@users.noreply.github.com>
2022-06-04 13:55:42 +0200

    Visual update

--- resources/js/assets.js
+++ resources/js/assets.js
@@ -1,10 +1,16 @@
 import {StandardLuminance, baseLayerLuminance} from "./bundles/fluentWebComponents.js";
 
 const setColorOnAssets = async () => {
-  if(userPreferences.colorMode === "dark")
-    baseLayerLuminance.setValueFor(document.body, StandardLuminance.DarkMode);
-  else
-    baseLayerLuminance.setValueFor(document.body, StandardLuminance.LightMode);
+  if(userPreferences.colorMode === "dark"){
+    baseLayerLuminance.setValueFor(document.body, 0.15);
+    baseLayerLuminance.setValueFor(document.getElementById("topEditorPanel-nav"), 0.20);
+    baseLayerLuminance.setValueFor(document.getElementById("topSettingsPanel-nav"), 0.20);
+  }
+  else{
+    baseLayerLuminance.setValueFor(document.body, 0.95);
+    baseLayerLuminance.setValueFor(document.getElementById("topEditorPanel-nav"), 1);
+    baseLayerLuminance.setValueFor(document.getElementById("topSettingsPanel-nav"), 1);
+  }
 }
 
 if(NL_LOADED.includes("COLOR") === true)
