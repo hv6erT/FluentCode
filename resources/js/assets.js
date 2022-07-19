ac33d3a323b1042ee781be2f771445790b0f5f3b
hv6erT <92681087+hv6erT@users.noreply.github.com>
2022-07-19 22:14:27 +0200

    Update assets.js
    Update nodes naming, remove unused code

--- resources/js/assets.js
+++ resources/js/assets.js
@@ -3,13 +3,11 @@ import {baseLayerLuminance} from "./bundles/fluentWebComponents.js";
 const setColorOnAssets = async () => {
   if(userPreferences.colorMode === "dark"){
     baseLayerLuminance.setValueFor(document.body, 0.15);
-    baseLayerLuminance.setValueFor(document.getElementById("topAppPanel-nav"), 0.20);
-    baseLayerLuminance.setValueFor(document.getElementById("topSettingsPanel-nav"), 0.20);
+    baseLayerLuminance.setValueFor(document.getElementById("topNav-fluent-toolbar"), 0.20);
   }
   else{
     baseLayerLuminance.setValueFor(document.body, 0.95);
-    baseLayerLuminance.setValueFor(document.getElementById("topAppPanel-nav"), 1);
-    baseLayerLuminance.setValueFor(document.getElementById("topSettingsPanel-nav"), 1);
+    baseLayerLuminance.setValueFor(document.getElementById("topNav-fluent-toolbar"), 1);
   }
 }
 
